00ef5f3e29db9e3e8bffb560681b2fa15848d025
Charles <szinubuntu@gmail.com>
2018-08-31 11:05:44 -0400

    Add basic player death and game restart support

--- objects/player.go
+++ objects/player.go
@@ -127,3 +127,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "extinguish_sound"
+  type: "sound"
+  data: "sound: \"/assets/sounds/sfx_explosion3.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
